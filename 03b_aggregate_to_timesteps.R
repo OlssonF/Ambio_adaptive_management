76c0991a3baa9613021eceef83c35ccce04d2bb2
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-05-09 08:54:27 -0400

    redo Q + AT scenario

--- 03b_aggregate_to_timesteps.R
+++ 03b_aggregate_to_timesteps.R
@@ -160,6 +160,156 @@ select(seasonyear_summary_e,  c(season, Q_change, T_change)) %>%# extract dateti
 #==========================================================#
 
 
+# ========= 4. change_Q_AT ========
+scenario <- 'change_Q_AT'
+# will have one of these for each timstep summary (daily, monthly, seasonal)
+# run for each metric
+metrics <- gsub("Hourly_", "", 
+                gsub(".txt", "", 
+                     list.files(file.path(out_dir, scenario, "Summaries"), pattern = "Hourly")))
+
+for (metric in metrics) {
+  metric_hourly <- readr::read_delim(file.path(out_dir, scenario, "Summaries", paste0("Hourly_",
+                                                                                      metric, 
+                                                                                      ".txt")), show_col_types = F)
+  
+  metric_daily <- metric_hourly %>%
+    mutate(date = ymd(format(datetime, "%Y-%m-%d"))) %>%
+    group_by(date) %>%
+    summarise_if(is.numeric, mean) %>%
+    pivot_longer(cols = - date, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  
+  # daily summary
+  if (!exists("daily_summary_d")) {
+    daily_summary_d <- metric_daily 
+  } else {
+    daily_summary_d <- full_join(daily_summary_d, metric_daily, by = c("T_change","Q_change", "date"))
+  }
+  
+  metric_yearmon <- metric_hourly %>%
+    mutate(month = as.yearmon(format(datetime, "%Y-%m"))) %>%
+    group_by(month) %>%
+    summarise_if(is.numeric, mean)%>%
+    pivot_longer(cols = - month, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  # yearmon summary
+  if (!exists("yearmon_summary_d")) {
+    yearmon_summary_d <- metric_yearmon 
+  } else {
+    yearmon_summary_d <- full_join(yearmon_summary_d, metric_yearmon, by = c("T_change","Q_change", "month"))
+  }
+  
+  metric_monthly <- metric_hourly %>%
+    mutate(month = month(datetime)) %>%
+    group_by(month) %>%
+    summarise_if(is.numeric, mean) %>%
+    pivot_longer(cols = - month, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  
+  # monthly summary
+  if (!exists("monthly_summary_d")) {
+    monthly_summary_d <- metric_monthly 
+  } else {
+    monthly_summary_d <- full_join(monthly_summary_d, metric_monthly, by = c("T_change","Q_change", "month"))
+  }
+  
+  metric_seasonal <- metric_hourly %>%
+    mutate(season = as.season(datetime)) %>%
+    group_by(season) %>%
+    summarise_if(is.numeric, mean) %>%
+    pivot_longer(cols = - season, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  
+  # seasonal summary
+  if (!exists("seasonal_summary_d")) {
+    seasonal_summary_d <- metric_seasonal 
+  } else {
+    seasonal_summary_d <- full_join(seasonal_summary_d, metric_seasonal, by = c("T_change","Q_change", "season"))
+  }
+  
+  metric_seasonyear <- metric_hourly %>%
+    mutate(season = as.seasonyear(datetime)) %>%
+    group_by(season) %>%
+    summarise_if(is.numeric, mean) %>%
+    pivot_longer(cols = - season, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  
+  # seasonyear summary
+  if (!exists("seasonyear_summary_d")) {
+    seasonyear_summary_d <- metric_seasonyear 
+  } else {
+    seasonyear_summary_d <- full_join(seasonyear_summary_d, metric_seasonyear, by = c("T_change","Q_change", "season"))
+  }
+  
+  metric_jday <- metric_hourly %>%
+    mutate(jday = yday(datetime)) %>%
+    group_by(jday) %>%
+    summarise_if(is.numeric, mean) %>%
+    pivot_longer(cols = - jday, 
+                 names_to = c("Q_change","T_change"),
+                 names_prefix = "Q_",
+                 names_sep = "_T_",
+                 values_to = metric)
+  
+  # jday summary
+  if (!exists("jday_summary_d")) {
+    jday_summary_d <- metric_jday 
+  } else {
+    jday_summary_d <- full_join(jday_summary_d, metric_jday, by = c("T_change","Q_change", "jday"))
+  }
+  
+  
+  print(metric)
+}
+
+# write the output to delim files
+select(daily_summary_d, c(date, Q_change, T_change)) %>% # extract ID not to be rounded
+  bind_cols(., round(daily_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_daily.txt"),
+                     delim = "\t")
+
+select(jday_summary_d, c(jday, Q_change, T_change)) %>%# extract datetime
+  bind_cols(., round(jday_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_jday.txt"),
+                     delim = "\t")
+
+select(monthly_summary_d, c(month, Q_change, T_change)) %>%# extract datetime
+  bind_cols(., round(monthly_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_monthly.txt"),
+                     delim = "\t")
+
+select(yearmon_summary_d, c(month, Q_change, T_change)) %>%# extract datetime
+  bind_cols(., round(yearmon_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_yearmon.txt"),
+                     delim = "\t")
+
+select(seasonyear_summary_d,  c(season, Q_change, T_change)) %>%# extract datetime
+  bind_cols(., round(seasonyear_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_seasonyear.txt"),
+                     delim = "\t")
+
+select(seasonal_summary_d,  c(season, Q_change, T_change)) %>%# extract datetime
+  bind_cols(., round(seasonal_summary_d[,metrics], 4)) %>% # rounded numeric columns
+  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_seasonal.txt"),
+                     delim = "\t")
+#==========================================================#
+
 # ========= 1. change_AT ========
 scenario <- 'change_AT'
 # will have one of these for each timstep summary (daily, monthly, seasonal)
@@ -552,130 +702,4 @@ select(seasonal_summary_c, c(season, Q_change)) %>%# extract datetime
 
 
 
-# ========= 4. change_Q_AT ========
-scenario <- 'change_Q_AT'
-# will have one of these for each timstep summary (daily, monthly, seasonal)
-# run for each metric
-metrics <- gsub("Hourly_", "", 
-                gsub(".txt", "", 
-                     list.files(file.path(out_dir, scenario, "Summaries"), pattern = "Hourly")))
 
-for (metric in metrics) {
-  metric_hourly <- readr::read_delim(file.path(out_dir, scenario, "Summaries", paste0("Hourly_",
-                                                                                      metric, 
-                                                                                      ".txt")), show_col_types = F)
-  
-  metric_daily <- metric_hourly %>%
-    mutate(date = ymd(format(datetime, "%Y-%m-%d"))) %>%
-    group_by(date) %>%
-    summarise_if(is.numeric, mean) %>%
-    pivot_longer(cols = - date, 
-                 names_to = c("Q_change","T_change"),
-                 names_prefix = "Q_",
-                 names_sep = "_T_",
-                 values_to = metric)
-  
-  # daily summary
-  if (!exists("daily_summary_d")) {
-    daily_summary_d <- metric_daily 
-  } else {
-    daily_summary_d <- full_join(daily_summary_d, metric_daily, by = c("T_change","Q_change", "date"))
-  }
-  
-  metric_yearmon <- metric_hourly %>%
-    mutate(month = as.yearmon(format(datetime, "%Y-%m"))) %>%
-    group_by(month) %>%
-    summarise_if(is.numeric, mean)%>%
-    pivot_longer(cols = - month, 
-                 names_to = c("Q_change","T_change"),
-                 names_prefix = "Q_",
-                 names_sep = "_T_",
-                 values_to = metric)
-  # yearmon summary
-  if (!exists("yearmon_summary_d")) {
-    yearmon_summary_d <- metric_yearmon 
-  } else {
-    yearmon_summary_d <- full_join(yearmon_summary_d, metric_yearmon, by = c("T_change","Q_change", "month"))
-  }
-  
-  metric_monthly <- metric_hourly %>%
-    mutate(month = month(datetime)) %>%
-    group_by(month) %>%
-    summarise_if(is.numeric, mean) %>%
-    pivot_longer(cols = - month, 
-                 names_to = c("Q_change","T_change"),
-                 names_prefix = "Q_",
-                 names_sep = "_T_",
-                 values_to = metric)
-  
-  # monthly summary
-  if (!exists("monthly_summary_d")) {
-    monthly_summary_d <- metric_monthly 
-  } else {
-    monthly_summary_d <- full_join(monthly_summary_d, metric_monthly, by = c("T_change","Q_change", "month"))
-  }
-  
-  metric_seasonal <- metric_hourly %>%
-    mutate(season = as.season(datetime)) %>%
-    group_by(season) %>%
-    summarise_if(is.numeric, mean) %>%
-    pivot_longer(cols = - season, 
-                 names_to = c("Q_change","T_change"),
-                 names_prefix = "Q_",
-                 names_sep = "_T_",
-                 values_to = metric)
-  
-  # seasonal summary
-  if (!exists("seasonal_summary_d")) {
-    seasonal_summary_d <- metric_seasonal 
-  } else {
-    seasonal_summary_d <- full_join(seasonal_summary_d, metric_seasonal, by = c("T_change","Q_change", "season"))
-  }
-  
-  metric_jday <- metric_hourly %>%
-    mutate(jday = yday(datetime)) %>%
-    group_by(jday) %>%
-    summarise_if(is.numeric, mean) %>%
-    pivot_longer(cols = - jday, 
-                 names_to = c("Q_change","T_change"),
-                 names_prefix = "Q_",
-                 names_sep = "_T_",
-                 values_to = metric)
-  
-  # jday summary
-  if (!exists("jday_summary_d")) {
-    jday_summary_d <- metric_jday 
-  } else {
-    jday_summary_d <- full_join(jday_summary_d, metric_jday, by = c("T_change","Q_change", "jday"))
-  }
-  
-  
-  print(metric)
-}
-
-# write the output to delim files
-select(daily_summary_d, c(date, Q_change, T_change)) %>% # extract ID not to be rounded
-  bind_cols(., round(daily_summary_d[,metrics], 4)) %>% # rounded numeric columns
-  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_daily.txt"),
-                     delim = "\t")
-
-select(jday_summary_d, c(date, Q_change, T_change)) %>%# extract datetime
-  bind_cols(., round(jday_summary_d[,metrics], 4)) %>% # rounded numeric columns
-  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_jday.txt"),
-                     delim = "\t")
-
-select(monthly_summary_d, c(date, Q_change, T_change)) %>%# extract datetime
-  bind_cols(., round(monthly_summary_d[,metrics], 4)) %>% # rounded numeric columns
-  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_monthly.txt"),
-                     delim = "\t")
-
-select(yearmon_summary_d, c(date, Q_change, T_change)) %>%# extract datetime
-  bind_cols(., round(yearmon_summary_d[,metrics], 4)) %>% # rounded numeric columns
-  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_yearmon.txt"),
-                     delim = "\t")
-
-select(seasonal_summary_d,  c(date, Q_change, T_change)) %>%# extract datetime
-  bind_cols(., round(seasonal_summary_d[,metrics], 4)) %>% # rounded numeric columns
-  readr::write_delim(., file.path(out_dir, scenario, "Summaries", "metric_summary_seasonal.txt"),
-                     delim = "\t")
-#==========================================================#
--- 03c_calculate_percentage_and_absolute_changes.R
+++ 03c_calculate_percentage_and_absolute_changes.R
@@ -111,6 +111,94 @@ for (j in 1:nrow(times_use)) {
 #======================================================#
 
 
+# ============ 4. change_Q_AT ======================
+# files to use for looking at changes
+# these are aggreagated to different timesteps
+scenario <- 'change_Q_AT'
+summaries <- list.files(path = file.path(out_dir, scenario, 'Summaries'),
+                        pattern = "metric_summary")
+
+
+#read in all the df
+for (file in summaries) {
+  temp <- readr::read_delim(file.path(out_dir, scenario, 'Summaries', file), show_col_types = F)
+  assign(gsub(".txt", "_summary", gsub("metric_summary_", "", file)),temp)
+}
+
+# extract the baseline summary for each timestep
+for (i in timesteps$time) {
+  t <- get(paste0(i, "_summary")) %>%
+    filter(Q_change == 1 & T_change == 0)
+  assign(paste0(i, "_base"), t)
+}
+
+# for each timestep it extracts the baseline and summary tables
+
+times_use <- timesteps |> 
+  filter(time %in% c("seasonal", "seasonyear"))
+
+
+
+# for each timestep it extracts the baseline and summary tables
+# then calculates the percentage and absolute changes for each row
+for (j in 4:nrow(timesteps)) {
+  summary <- get(paste0(timesteps$time[j], "_summary")) # extract the summary table for the timestep
+  base <- get(paste0(timesteps$time[j], "_base")) # extract the baseline table for the timestep
+  
+  #create empty df to put the values in
+  assign(x = "temp_perc_change", summary)
+  assign(x = "temp_abs_change", summary)
+  
+  for (i in 1:nrow(summary)) {
+    # calculate the pecentage chage based on the functions above
+    # assings values to the relvent columns, based on names
+    temp_perc_change[i, metrics] <- perc.change.v(summary[i,], 
+                                                  t = as.character(timesteps$val[j]),
+                                                  base_df = base)
+    
+    # calculate the absolute chage based on the functions above
+    temp_abs_change[i,metrics] <- abs.change.v(summary[i,], 
+                                               t = as.character(timesteps$val[j]),
+                                               base_df = base)
+    
+    
+    # write the temporary df
+    select(temp_perc_change[i,], c(timesteps$val[j], Q_change, T_change)) %>% # extract id columns
+      bind_cols(., round(temp_perc_change[i,metrics], 3)) %>% 
+      write.table(., paste0("./d - airT + inflowQ/Summaries/percent_change_",timesteps$time[j], ".txt"),
+                  sep = "\t", quote =F, row.names = F,
+                  append = T, # append to existing file
+                  col.names = ifelse(file.exists(paste0("./d - airT + inflowQ/Summaries/percent_change_",
+                                                        timesteps$time[j]
+                                                        ,".txt")) ==
+                                       T, # if thhe file already exists
+                                     F, # omit the column names
+                                     T))
+    
+    select(temp_abs_change[i,], c(timesteps$val[j], Q_change, T_change)) %>% # extract id columns
+      bind_cols(., round(temp_abs_change[i,metrics], 3)) %>% 
+      write.table(., paste0("./d - airT + inflowQ/Summaries/abs_change_",timesteps$time[j], ".txt"),
+                  sep = "\t", quote =F, row.names = F,
+                  append = T, # append to existing file
+                  col.names =ifelse(file.exists(paste0("./d - airT + inflowQ/Summaries/abs_change_",
+                                                       timesteps$time[j],
+                                                       ".txt")) ==
+                                      T, # if thhe file already exists
+                                    F, # omit the column names
+                                    T))
+    
+    print(i)
+    Sys.sleep(0.01)
+    flush.console()
+  }
+  
+  
+  print(timesteps$time[j])
+  Sys.sleep(0.01)
+  flush.console()
+}
+#===============================================================#
+
 # ============ 1. change_AT ======================
 # files to use for looking at changes
 # these are aggreagated to different timesteps
@@ -304,82 +392,4 @@ for (j in 1:nrow(timesteps)) {
 #===============================================================#
 
 
-# ============ 4. change_Q_AT ======================
-# files to use for looking at changes
-# these are aggreagated to different timesteps
-summaries <- list.files(path = "./d - airT + inflowQ/Summaries/",
-                        pattern = "metric_summary")
-
-#read in all the df
-for (file in summaries) {
-  temp <- read.delim(paste0("./d - airT + inflowQ/Summaries/", file))
-  assign(gsub(".txt", "_summary", gsub("metric_summary_", "", file)),temp)
-}
-
-# extract the baseline summary for each timestep
-for (i in timesteps$time) {
-  t <- get(paste0(i, "_summary")) %>%
-    filter(Q_change == 1 & T_change == 0)
-  assign(paste0(i, "_base"),t)
-}
-
-# for each timestep it extracts the baseline and summary tables
-# then calculates the percentage and absolute changes for each row
-for (j in 4:nrow(timesteps)) {
-  summary <- get(paste0(timesteps$time[j], "_summary")) # extract the summary table for the timestep
-  base <- get(paste0(timesteps$time[j], "_base")) # extract the baseline table for the timestep
-  
-  #create empty df to put the values in
-  assign(x = "temp_perc_change", summary)
-  assign(x = "temp_abs_change", summary)
-  
-  for (i in 1:nrow(summary)) {
-    # calculate the pecentage chage based on the functions above
-    # assings values to the relvent columns, based on names
-    temp_perc_change[i, metrics] <- perc.change.v(summary[i,], 
-                                                           t = as.character(timesteps$val[j]),
-                                                           base_df = base)
-    
-    # calculate the absolute chage based on the functions above
-    temp_abs_change[i,metrics] <- abs.change.v(summary[i,], 
-                                                        t = as.character(timesteps$val[j]),
-                                                        base_df = base)
-    
-    
-    # write the temporary df
-    select(temp_perc_change[i,], c(timesteps$val[j], Q_change, T_change)) %>% # extract id columns
-      bind_cols(., round(temp_perc_change[i,metrics], 3)) %>% 
-      write.table(., paste0("./d - airT + inflowQ/Summaries/percent_change_",timesteps$time[j], ".txt"),
-                  sep = "\t", quote =F, row.names = F,
-                  append = T, # append to existing file
-                  col.names = ifelse(file.exists(paste0("./d - airT + inflowQ/Summaries/percent_change_",
-                                                       timesteps$time[j]
-                                                       ,".txt")) ==
-                                      T, # if thhe file already exists
-                                    F, # omit the column names
-                                    T))
-    
-    select(temp_abs_change[i,], c(timesteps$val[j], Q_change, T_change)) %>% # extract id columns
-      bind_cols(., round(temp_abs_change[i,metrics], 3)) %>% 
-      write.table(., paste0("./d - airT + inflowQ/Summaries/abs_change_",timesteps$time[j], ".txt"),
-                  sep = "\t", quote =F, row.names = F,
-                  append = T, # append to existing file
-                  col.names =ifelse(file.exists(paste0("./d - airT + inflowQ/Summaries/abs_change_",
-                                                timesteps$time[j],
-                                                ".txt")) ==
-                                      T, # if thhe file already exists
-                                    F, # omit the column names
-                                    T))
-    
-    print(i)
-    Sys.sleep(0.01)
-    flush.console()
-  }
-  
-  
-  print(timesteps$time[j])
-  Sys.sleep(0.01)
-  flush.console()
-}
-#===============================================================#
 
