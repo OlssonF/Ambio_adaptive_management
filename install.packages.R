990374f71f5a0ebb10b64090783859c793cc2a4d
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-11-27 16:50:25 -0500

    install relevent packages

---
+++ install.packages.R
@@ -0,0 +1,5 @@
+# packages to install
+install.packages('tidyverse','lubridate', 'ggpubr', 'cowplot', 'here', 'zoo')
+install.packages('remotes')
+remotes::install_github('aemon-j/LakeEnsemblR')
+install.packages('rLakeAnalyzer')
