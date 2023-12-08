cd8f29afdb2714a757985f5d79f9f49d96811fb0
Freya E Olsson <freyao@tc011.cm.cluster>
2023-12-08 16:27:26 -0500

    update download archive

--- download_output.R
+++ download_output.R
@@ -1,6 +1,18 @@
 directory <- here::here()
 
-download.file(url = "https://sandbox.zenodo.org/records/6078/files/Experiment_output.zip?download=1",
-              destfile = file.path(directory,"GOTM",'Output', "Experiment_output.zip"))
+# GOTM scenarios output
+if (!dir.exists(file.path(file.path(directory,"GOTM",'Output')))) {
+  dir.create(file.path(file.path(directory,"GOTM",'Output')), recursive = T)
+}
 
+#this download can take a while to download
+download.file(url = "https://sandbox.zenodo.org/records/7282/files/Experiment_output.zip?download=1",
+              destfile = file.path(directory,"GOTM",'Output', "Experiment_output.zip"), method = 'curl')
+
+setwd(file.path(file.path(directory,"GOTM",'Output')))
 unzip(file.path(directory,"GOTM",'Output', "Experiment_output.zip"))
+
+
+# output from GOTM based on observations
+download.file(url = "https://sandbox.zenodo.org/records/7282/files/Obs_z.txt?download=1",
+              destfile = file.path(directory,"GOTM",'Output', 'Obs_z.txt'), method = 'curl')
