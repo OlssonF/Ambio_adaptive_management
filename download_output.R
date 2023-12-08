e2f6d6acd27a5fa1fab81502237a67bb5f001daa
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-12-08 09:56:53 -0500

    change download location

--- download_output.R
+++ download_output.R
@@ -1,5 +1,6 @@
 directory <- here::here()
 
-download.file(url = "https://zenodo.org/record/8136961/files/Experiment_output.zip?download=1",
-              destfile = file.path(directory,"GOTM", "Experiment_output.zip"), method = "curl")
-unzip(file.path(directory,, "GOTM", "Experiment_output.zip"))
+download.file(url = "https://sandbox.zenodo.org/records/6078/files/Experiment_output.zip?download=1",
+              destfile = file.path(directory,"GOTM",'Output', "Experiment_output.zip"))
+
+unzip(file.path(directory,"GOTM",'Output', "Experiment_output.zip"))
