abb1df16cf6482bac11e64e878e65df046bfe205
LAPTOP-FO87M896\freya <freya.olsson@outlook.com>
2023-12-08 19:00:54 -0500

    update zenodo links

--- download_output.R
+++ download_output.R
@@ -6,7 +6,7 @@ if (!dir.exists(file.path(file.path(directory,"GOTM",'Output')))) {
 }
 
 #this download can take a while to download
-download.file(url = "https://sandbox.zenodo.org/records/7282/files/Experiment_output.zip?download=1",
+download.file(url = "https://zenodo.org/records/10309370/files/Experiment_output.zip?download=1",
               destfile = file.path(directory,"GOTM",'Output', "Experiment_output.zip"), method = 'curl')
 
 setwd(file.path(file.path(directory,"GOTM",'Output')))
@@ -14,5 +14,5 @@ unzip(file.path(directory,"GOTM",'Output', "Experiment_output.zip"))
 
 
 # output from GOTM based on observations
-download.file(url = "https://sandbox.zenodo.org/records/7282/files/Obs_z.txt?download=1",
-              destfile = file.path(directory,"GOTM",'Output', 'Obs_z.txt'), method = 'curl')
+download.file(url = "https://zenodo.org/records/10309370/files/Mod_z.txt?download=1",
+              destfile = file.path(directory,"GOTM",'Output', 'Mod_z.txt'), method = 'curl')
